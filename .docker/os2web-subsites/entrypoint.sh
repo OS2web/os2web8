25f3d97d6b99e80559f52b9e80119aacc3ab99ad
andriyun <andriy.yun@gmail.com>
2022-04-05 23:15:24 +0200

    BKDK-521 Adjusted os2subsites docker config

--- .docker/os2web-subsites/entrypoint.sh
+++ .docker/os2web-subsites/entrypoint.sh
@@ -3,4 +3,20 @@
 # Starting cron service.
 service cron start
 
+# Check basic file structure for subsites creator
+if [ ! -d "/opt/drupal/web/sites/default" ]
+then
+  echo "Default sites folder doesn't exists. Create folder and standard files"
+  mkdir -p /opt/drupal/web/sites/default/files
+  cp -f /opt/drupal/.docker/os2web-subsites/settings/default.settings.php /opt/drupal/web/sites/default/default.settings.php
+  cp -f /opt/drupal/.docker/os2web-subsites/settings/default.settings.php /opt/drupal/web/sites/default/settings.php
+  echo 'include $app_root . "/" . $site_path . "/settings.local.php";' >> /opt/drupal/web/sites/default/settings.php
+  cp -f /opt/drupal/.docker/os2web-subsites/settings/settings.local.php /opt/drupal/web/sites/default/settings.local.php
+  chmod 755 /opt/drupal/web/sites/default /opt/drupal/web/sites/default/settings.php /opt/drupal/web/sites/default/settings.local.php
+  chown -R www-data:www-data /opt/drupal/web/sites
+fi
+
+# Copying shared settings file
+cp -f /opt/drupal/.docker/os2web-subsites/settings/shared.settings.php /opt/drupal/web/sites/shared.settings.php
+
 exec "$@"
