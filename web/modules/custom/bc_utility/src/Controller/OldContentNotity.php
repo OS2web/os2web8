2d244de04c85318111c5afcbb96d271c0f401cfb
Henrik Tholle <ht@bellcom.dk>
2022-06-22 17:00:27 +0200

    Cleanup

--- web/modules/custom/bc_utility/src/Controller/OldContentNotity.php
+++ web/modules/custom/bc_utility/src/Controller/OldContentNotity.php
@@ -9,8 +9,6 @@ Class OldContentNotity
 
   public static function handler()
   {
-    // \Drupal::logger('bc_utility')->notice('bellcom OldContentNotify start');
-
     $outdated = array();
     $nodes = \Drupal::entityTypeManager()
       ->getStorage('node')
@@ -41,7 +39,6 @@ Class OldContentNotity
                 $user = \Drupal\user\Entity\User::load($userId);
                 if (!empty($user->get('mail')->value))
                 {
-
                   if (empty($outdated[$user->id()]))
                   {
                     $outdated[$user->id()]['name'] = $user->get('name')->value;
@@ -59,41 +56,51 @@ Class OldContentNotity
           }
         }
       }
-
     }
 
     if (count($outdated))
     {
-
       // TODO get mail from Content Moderation notification
 //      $id = 'email_brugere_ved_foraeldede_sider';
 //      $ContentModerationNote = ContentModerationNotification::load($id);
 //      $subject = $ContentModerationNote->getSubject();
 //      $body = $ContentModerationNote->getMessage();
 
+      $mailManager = \Drupal::service('plugin.manager.mail');
       $subject = 'En side er forældet.';
+      $module = 'bc_utiltity';
+      $key = 'old_article';
+      $langcode = 'da';
+      $send = true;
       $body = null;
+      $params = array(
+        'subject' => 'A page is outdated',
+        'body' => null,
+        'headers' => array(
+          'Content-Type' => 'text/html; charset=UTF-8; format=flowed; delsp=yes;'
+        )
+      );
+
       foreach ( $outdated AS $user )
       {
         $body = '<p>Hej ' . $user['name'] . '</p>';
-
         foreach ( $user['outdated'] as $page )
         {
-          $body .= '<p>siden ' . $page['title'] . ' er forældet</p>';
-          $body .= '<p><a href="' . $page['link'] . ' target="_blank">Link</a></p><br>';
-
+          $body .= '<p>Siden "' . $page['title'] . '" er forældet. <a href="' . $page['link'] . ' target="_blank">Link</a></p>';
         }
 
-        $mailManager = \Drupal::service('plugin.manager.mail');
-        $module = 'bc_utiltity';
-        $key = 'create_article';
-        $to = $user['email'];
-        $params['message'] = $body;
-        $params['node_title'] = $subject;
-        $langcode = 'da';
-        $send = true;
+        $params['body'] = $body;
 
-        $result = $mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
+        $result = $mailManager->mail(
+          'bc_utility',
+          'old_article',
+          $user['email'],
+          $langcode,
+          $params,
+          null,
+          true
+        );
+
         if ($result['result'] !== true)
         {
           \Drupal::logger('bc_utility')->notice('old content email could not be sent to ' . $user['email']);
