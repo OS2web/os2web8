36c2e9c0b319df35e3a217e27f4297a8fef2a892
andriyun <andriy.yun@gmail.com>
2022-07-06 12:37:13 +0200

    Made ballerup subsites theme support banner slideshow

--- web/themes/custom/fds_bg_theme/src/javascripts/slideshow.js
+++ web/themes/custom/fds_bg_theme/src/javascripts/slideshow.js
@@ -18,4 +18,25 @@
       rewind: true,
     });
   }
+  var selector2 = '.field--name-field-os2web-page-paragraph-bann.field__items';
+
+  if (document.querySelector(selector2) !== null) {
+    // Run tiny slider.
+    tns({
+      container: selector2,
+      items: 1,
+      autoplay: false,
+      autoplayHoverPause: true,
+      gutter: 32,
+      rewind: true,
+      onInit: function () {
+        var items = $(selector2).find('.tns-item > .banner');
+        var height = 0;
+        for (var i = 0; i < items.length; i++) {
+          height = Math.max(height, $(items[i]).height());
+        }
+        items.height(height);
+      }
+    });
+  }
 })(jQuery, Drupal, drupalSettings);
