cb36c67b37d5e3a3533165c37d6d3f1e0ab88430
Simon L. Lange <sl@bellcom.dk>
2023-03-01 01:42:53 +0000

    Bellcom theme changes

--- web/themes/custom/subsites/bellcom_theme/assets/js/bundle.js
+++ web/themes/custom/subsites/bellcom_theme/assets/js/bundle.js
@@ -1,1 +1,14 @@
-!function(){"use strict";document.addEventListener("DOMContentLoaded",(function(){console.log("Webpack is working fine")}))}();
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Webpack is working fine');
+});
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoianMvYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFZOztBQUVaQSxRQUFRLENBQUNDLGdCQUFnQixDQUFDLGtCQUFrQixFQUFFLFlBQU07RUFDbERDLE9BQU8sQ0FBQ0MsR0FBRyxDQUFDLHlCQUF5QixDQUFDO0FBQ3hDLENBQUMsQ0FBQyxDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vQmVsbGNvbS8uL3NyYy9qcy9tYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIid1c2Ugc3RyaWN0J1xuXG5kb2N1bWVudC5hZGRFdmVudExpc3RlbmVyKCdET01Db250ZW50TG9hZGVkJywgKCkgPT4ge1xuICBjb25zb2xlLmxvZygnV2VicGFjayBpcyB3b3JraW5nIGZpbmUnKVxufSlcbiJdLCJuYW1lcyI6WyJkb2N1bWVudCIsImFkZEV2ZW50TGlzdGVuZXIiLCJjb25zb2xlIiwibG9nIl0sInNvdXJjZVJvb3QiOiIifQ==
