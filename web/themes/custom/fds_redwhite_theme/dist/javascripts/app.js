88ed01131ad2d0d05338afc86982aa085d5b7ba1
Simon L. Lange <sl@bellcom.dk>
2023-03-06 08:20:17 +0000

    Ringsted-645: Missing aria-label on submit buttons without text

--- web/themes/custom/fds_redwhite_theme/dist/javascripts/app.js
+++ web/themes/custom/fds_redwhite_theme/dist/javascripts/app.js
@@ -3249,11 +3249,18 @@ module.exports = function (button, expanded) {
 // Search.
 (function() {
   var searchInputs = document.querySelectorAll('.search-input__input');
+  var searchSubmits = document.querySelectorAll('.search-input__button');
 
   for (var i = 0; i < searchInputs.length; i++) {
     var input = searchInputs[i];
 
     input.setAttribute('aria-label', 'Indtast søgeord');
+  }
+
+  for (var j = 0; j < searchSubmits.length; i++) {
+    var submit = searchSubmits[j];
+
+    submit.setAttribute('aria-label', 'Søg');
   }
 })();
 
