5177deb5c37e87f45aeb337aa65c44a6b82949c9
Simon L. Lange <sl@bellcom.dk>
2023-03-06 14:00:08 +0000

    Bellcom theme: Animation library added, fontawesome added, navigation mobile started

--- web/themes/custom/subsites/bellcom_theme/src/js/main.js
+++ web/themes/custom/subsites/bellcom_theme/src/js/main.js
@@ -1,6 +1,15 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+  AOS.init();
+
 });
 
 
+const hamburgerBtn = document.querySelector('.hamburger');
+
+const menuElement = document.querySelector('.region--header');
+
+hamburgerBtn.addEventListener('click', () => {
+  menuElement.classList.add('is-open');
+});
