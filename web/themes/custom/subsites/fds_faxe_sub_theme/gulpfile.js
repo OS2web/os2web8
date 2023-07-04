5e6657861ce348402599b8b509943126624b5341
Henrik Tholle <ht@bellcom.dk>
2023-07-04 17:14:15 +0200

    FAXE-433 add sass to style

--- web/themes/custom/subsites/fds_faxe_sub_theme/gulpfile.js
+++ web/themes/custom/subsites/fds_faxe_sub_theme/gulpfile.js
@@ -2,14 +2,13 @@
 let compileConfig = {
     settings: require('./src/compile-settings.json')
   };
-  
+
   let gulpConfig = {
     settings: require('./src/gulp-settings.json')
   };
-  
-  
+
   const { src, dest, series, watch } = require('gulp');
-  const sass = require('gulp-sass');
+  const sass = require('gulp-sass')(require('sass'));
   const csso = require('gulp-csso');
   const include = require('gulp-file-include');
   const htmlmin = require('gulp-htmlmin');
@@ -17,9 +16,8 @@ let compileConfig = {
   const concat = require('gulp-concat');
   const autoprefixer = require('gulp-autoprefixer');
   const sync = require('browser-sync').create();
-  
-  
-  
+
+
   function html() {
     return src('src/**.html')
       .pipe(
@@ -34,7 +32,7 @@ let compileConfig = {
       )
       .pipe(dest('dist'));
   }
-  
+
   function scss() {
     return src(compileConfig.settings.styles)
       .pipe(sass())
@@ -47,17 +45,17 @@ let compileConfig = {
       .pipe(concat('stylesheet.css'))
       .pipe(dest('dist/stylesheets'));
   }
-  
-  
-  
-  
+
+
+
+
   function fonts() {
     return src(compileConfig.settings.fonts).pipe(dest('dist/fonts'));
   }
   function img() {
     return src('src/images/**/*').pipe(dest('dist/images'));
   }
-  
+
   function js() {
     return src(compileConfig.settings.javascripts)
       .pipe(include())
@@ -65,27 +63,26 @@ let compileConfig = {
       .pipe(dest('dist/javascripts'))
       .pipe(sync.stream());
   }
-  
+
   function clear() {
     return del('dist');
   }
-  
-  
-  
+
+
+
   function serve() {
     sync.init({
       proxy: gulpConfig.settings.options.proxy
     });
-  
+
     watch('**/*.+(twig|twig.html|tpl|tpl.php|html)', series(html)).on('change', sync.reload);
     watch('src/styles/**/**.scss', series(scss)).on('change', sync.reload);
     watch('src/styles/css/**/**.css', series(scss)).on('change', sync.reload);
     watch('src/images/**.**', series(img)).on('change', sync.reload);
     watch('src/javascripts/**/*.js', series(js)).on('change', sync.reload);
   }
-  
+
   exports.build = series(clear, scss, html, js, img, fonts);
   exports.serve = series(clear, scss, html, js, img, fonts, serve);
   exports.clear = clear;
-  
-  
+
