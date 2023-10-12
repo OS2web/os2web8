27dbe21e3db0b4200a5741fd009f79a2d101e458
Simon L. Lange <sl@bellcom.dk>
2023-10-12 11:41:57 +0000

    Fredericia: Slideshow fix

--- web/themes/custom/fds_fredericia_main_theme/js/slideshow.js
+++ web/themes/custom/fds_fredericia_main_theme/js/slideshow.js
@@ -3,86 +3,90 @@
  */
 
 // Slideshow popup.
-// (function() {
-//   function handleClick(event) {
-//     event.preventDefault();
+(function() {
+  function handleClick(event) {
+    event.preventDefault();
 
-//     var link = this;
-//     var image = link.querySelector('img');
-//     var text = image.getAttribute('title');
-//     var pathToImage = link.getAttribute('href');
-//     var wrapper = link.closest('.field--name-field-os2web-slideshow-image');
-//     var modalNodeElement = wrapper.querySelector('.modal');
+    var link = this;
+    var image = link.querySelector('img');
+    var text = image.getAttribute('title');
+    var pathToImage = link.getAttribute('href');
+    var wrapper = link.closest('.field--name-field-os2web-slideshow-image');
+    var modalNodeElement = wrapper.querySelector('.modal');
 
-//     createModal(modalNodeElement, text, pathToImage);
-//   }
+    createModal(modalNodeElement, text, pathToImage);
+  }
 
-//   function createModal(modalNodeElement, text, imagePath) {
-//     var modalId = modalNodeElement.getAttribute('id');
-//     var modalBody = modalNodeElement.querySelector('.modal__content');
-//     modalBody.innerHTML = ''; // Empty the modal content.
+  function createModal(modalNodeElement, text, imagePath) {
+    var modalId = modalNodeElement.getAttribute('id');
+    var modalBody = modalNodeElement.querySelector('.modal__content');
+    modalBody.innerHTML = ''; // Empty the modal content.
 
-//     // Create and add image.
-//     var image = document.createElement('img');
-//     image.src = imagePath;
-//     image.alt = text;
+    // Create and add image.
+    var image = document.createElement('img');
+    image.src = imagePath;
+    image.alt = text;
 
-//     modalBody.appendChild(image);
+    modalBody.appendChild(image);
 
-//     // Set text.
-//     var caption = document.createElement('h4');
-//     caption.innerText = text;
+    // Set text.
+    var caption = document.createElement('h4');
+    caption.innerText = text;
 
-//     modalBody.appendChild(caption);
+    modalBody.appendChild(caption);
 
-//     // Open modal.
-//     MicroModal.show(modalId);
-//   }
+    // Open modal.
+    MicroModal.show(modalId);
+  }
 
-//   var links = document.querySelectorAll('.field--name-field-os2web-slideshow-image .field__item a');
+  var links = document.querySelectorAll('.field--name-field-os2web-slideshow-image .field__item a');
 
-//   for (var i = 0; i < links.length; i++) {
-//     var link = links[i];
+  for (var i = 0; i < links.length; i++) {
+    var link = links[i];
 
-//     link.addEventListener('click', handleClick)
-//   }
-// })();
+    link.addEventListener('click', handleClick)
+  }
+})();
 
 // Tiny Slider slideshow img.
 (function() {
-  var selector = '.paragraph--type--os2web-slideshow';
+  var selector = '.field--name-field-os2web-slideshow-image .field__items';
 
-  // var transitionEndCallback = function (info, eventName) {
-  //   // Add tab
-  //   for (var i = 0; i < info.slideItems.length; i++) {
-  //     var slide = info.slideItems[i];
-  //     if (slide.classList.contains('tns-slide-active')) {
-  //       slide.querySelector('a').removeAttribute('tabindex');
-  //     }
-  //     else {
-  //       slide.querySelector('a').setAttribute('tabindex', '-1');
-  //     }
-  //   }
-  // }
+  var transitionEndCallback = function (info, eventName) {
+    // Add tab
+    for (var i = 0; i < info.slideItems.length; i++) {
+      var slide = info.slideItems[i];
+      var anchor = slide.querySelector('a');
+
+      if (anchor) { // Check if the anchor exists before accessing it
+        if (slide.classList.contains('tns-slide-active')) {
+          anchor.removeAttribute('tabindex');
+        } else {
+          anchor.setAttribute('tabindex', '-1');
+        }
+      }
+    }
+  }
 
   if (document.querySelector(selector) !== null) {
 
     // Run tiny slider.
-    tns({
+    var slider = tns({
       container: selector,
       items: 1,
-      autoplay: true,
+      autoplay: false,
       autoplayHoverPause: true,
-      mouseDrag: true,
-      gutter: 0,
+      gutter: 32,
       rewind: true,
-      controls: false,
-      nested: false,
+      responsive: {
+        576: {
+          items: 2,
+        },
+      },
     });
 
-    // // bind function to event
-    // slider.events.on('transitionEnd', transitionEndCallback);
-    // transitionEndCallback(slider.getInfo());
+    // bind function to event
+    slider.events.on('transitionEnd', transitionEndCallback);
+    transitionEndCallback(slider.getInfo());
   }
 })();
-
