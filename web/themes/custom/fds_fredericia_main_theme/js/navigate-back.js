d89d8624aa69bbbceb6fe74d7bcd6612daf2630c
Simon L. Lange <sl@bellcom.dk>
2023-10-18 10:02:28 +0000

    Fredericia: Navigate back js

--- web/themes/custom/fds_fredericia_main_theme/js/navigate-back.js
+++ web/themes/custom/fds_fredericia_main_theme/js/navigate-back.js
@@ -1,34 +1,21 @@
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.navigateBack = {
     attach: function (context, settings) {
+      // Store the current URL when it's not a search page.
+      if (window.location.href.indexOf('/s?sq=') === -1) {
+        localStorage.setItem('lastNonSearchPage', window.location.href);
+      }
+
       $('.search-close-btn', context).once('navigate-back').click(function (e) {
         e.preventDefault();
 
-        let referrer = document.referrer;
+        const storedURL = localStorage.getItem('lastNonSearchPage');
 
-        // If there's no referrer or it's external, go to the front page
-        if (!referrer || referrer.indexOf(window.location.hostname) === -1) {
-          window.location.href = drupalSettings.path.baseUrl;
-          return;
+        if (storedURL) {
+          window.location.href = storedURL;
+        } else {
+          window.location.href = drupalSettings.path.baseUrl; // Navigate to the front page
         }
-
-        // If the referrer is NOT the "/s" pattern, simply navigate back
-        if (referrer.indexOf('/s?sq=') === -1) {
-          window.history.back();
-          return;
-        }
-
-        // If we're here, it means the referrer is the "/s" pattern.
-        // We'll navigate back until we find a URL that doesn't match the "/s" pattern
-        let stepsBack = 1;
-        let previousUrl = window.history.go(-stepsBack);
-
-        while (previousUrl.indexOf('/s?sq=') !== -1 && previousUrl.indexOf(window.location.hostname) !== -1) {
-          stepsBack++;
-          previousUrl = window.history.go(-stepsBack);
-        }
-
-        window.history.go(-stepsBack);
       });
     }
   };
