0c41b5378a1088554c379865a29149bd29e9658a
Simon L. Lange <sl@bellcom.dk>
2023-10-18 09:34:20 +0000

    Fredericia: Navigate back button fix

--- web/themes/custom/fds_fredericia_main_theme/js/navigate-back.js
+++ web/themes/custom/fds_fredericia_main_theme/js/navigate-back.js
@@ -4,8 +4,14 @@
       $('.search-close-btn', context).once('navigate-back').click(function (e) {
         e.preventDefault();
 
-        // Check if the previous page was within your site and is not a paginated page
-        if (document.referrer.indexOf(window.location.hostname) !== -1 && !document.referrer.match(/page=\d+/)) {
+        let referrer = document.referrer;
+        let currentLocation = window.location.href;
+
+        // Check if the referrer is the search page without pagination
+        let isSearchPage = referrer.indexOf('/s?sq=') !== -1 && referrer.indexOf('page=') === -1;
+
+        // Check if the previous page was within your site and is not a paginated page or the search page
+        if (referrer.indexOf(window.location.hostname) !== -1 && !isSearchPage && !referrer.match(/page=\d+/)) {
           window.history.back();
         } else {
           window.location.href = drupalSettings.path.baseUrl; // Navigate to the front page
