df7e6d41e2caa65a1f857d9ce3f928755ef31e00
Simon L. Lange <sl@bellcom.dk>
2023-10-18 09:16:33 +0000

    Fredericia: Added functionality to Search close button

--- web/themes/custom/fds_fredericia_main_theme/dist/javascripts/app.js
+++ web/themes/custom/fds_fredericia_main_theme/dist/javascripts/app.js
@@ -3651,3 +3651,20 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 })(jQuery);
+
+(function ($, Drupal, drupalSettings) {
+  Drupal.behaviors.navigateBack = {
+    attach: function (context, settings) {
+      $('.search-close-btn', context).once('navigate-back').click(function (e) {
+        e.preventDefault();
+
+        // Check if the previous page was within your site and is not a paginated page
+        if (document.referrer.indexOf(window.location.hostname) !== -1 && !document.referrer.match(/page=\d+/)) {
+          window.history.back();
+        } else {
+          window.location.href = drupalSettings.path.baseUrl; // Navigate to the front page
+        }
+      });
+    }
+  };
+})(jQuery, Drupal, drupalSettings);
