e8631b1be580765b1b6da501aca837751509d0f6
Simon L. Lange <sl@bellcom.dk>
2023-10-18 09:47:13 +0000

    Fredericia: More conditionals

--- web/themes/custom/fds_fredericia_main_theme/js/navigate-back.js
+++ web/themes/custom/fds_fredericia_main_theme/js/navigate-back.js
@@ -1,24 +1,34 @@
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.navigateBack = {
     attach: function (context, settings) {
-      $('.search-close-btn', context).once('navigate-back').click(function (e) {
+      $('.navigate-back-button', context).once('navigate-back').click(function (e) {
         e.preventDefault();
 
         let referrer = document.referrer;
-        let currentLocation = window.location.href;
 
-        // Check if the referrer is the search page without pagination
-        let isSearchPage = referrer.indexOf('/s?sq=') !== -1 && referrer.indexOf('page=') === -1;
-
-        // Check if the previous page was within your site and is not a paginated page or the search page
-        if (referrer.indexOf(window.location.hostname) !== -1 && !isSearchPage && !referrer.match(/page=\d+/)) {
-          window.history.back();
-        } else if (referrer.indexOf(window.location.hostname) !== -1 && (isSearchPage || referrer.match(/page=\d+/))) {
-          // If the user came from another internal page (not search or pagination), send them back to that page
-          window.history.go(-(window.history.length - 1));
-        } else {
-          window.location.href = drupalSettings.path.baseUrl; // Navigate to the front page
+        // If there's no referrer or it's external, go to the front page
+        if (!referrer || referrer.indexOf(window.location.hostname) === -1) {
+          window.location.href = drupalSettings.path.baseUrl;
+          return;
         }
+
+        // If the referrer is NOT the "/s" pattern, simply navigate back
+        if (referrer.indexOf('/s?sq=') === -1) {
+          window.history.back();
+          return;
+        }
+
+        // If we're here, it means the referrer is the "/s" pattern.
+        // We'll navigate back until we find a URL that doesn't match the "/s" pattern
+        let stepsBack = 1;
+        let previousUrl = window.history.go(-stepsBack);
+
+        while (previousUrl.indexOf('/s?sq=') !== -1 && previousUrl.indexOf(window.location.hostname) !== -1) {
+          stepsBack++;
+          previousUrl = window.history.go(-stepsBack);
+        }
+
+        window.history.go(-stepsBack);
       });
     }
   };
