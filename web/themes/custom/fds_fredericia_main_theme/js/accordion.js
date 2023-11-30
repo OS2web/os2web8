6967f8634b6c34928e3a01cb703235e6bba2ec7d
Simon L. Lange <sl@bellcom.dk>
2023-11-30 09:58:02 +0000

    Fredericia: Only one accordion open at a time

---
+++ web/themes/custom/fds_fredericia_main_theme/js/accordion.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var accordionButtons = document.querySelectorAll('.accordion-button');
+
+  function hideAllContents() {
+    var allContents = document.querySelectorAll('.accordion-content.opened');
+    allContents.forEach(function(content) {
+      content.classList.remove('opened');
+      // Handle aria-expanded for accessibility
+      content.previousElementSibling.setAttribute('aria-expanded', 'false');
+    });
+  }
+
+  accordionButtons.forEach(function(button) {
+    button.addEventListener('click', function(event) {
+      console.log('clicked');
+      var contentId = button.getAttribute('aria-controls');
+      var content = document.getElementById(contentId);
+      var isOpening = !content.classList.contains('opened');
+
+      // Hide all other contents
+      hideAllContents();
+
+      if (isOpening) {
+        content.classList.add('opened');
+        // Handle aria-expanded for accessibility
+        button.setAttribute('aria-expanded', 'true');
+      }
+    });
+  });
+});
