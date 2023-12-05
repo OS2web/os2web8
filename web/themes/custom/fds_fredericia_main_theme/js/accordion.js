159b2c0fe304243b1dff0a93e988e3833f927b28
Simon L. Lange <sl@bellcom.dk>
2023-12-05 10:51:44 +0000

    Fredericia: Redefine accordion logic

--- web/themes/custom/fds_fredericia_main_theme/js/accordion.js
+++ web/themes/custom/fds_fredericia_main_theme/js/accordion.js
@@ -1,28 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
   var accordionButtons = document.querySelectorAll('.accordion-button');
 
-  function hideAllContents() {
-    var allContents = document.querySelectorAll('.accordion-content.opened');
+  function hideAllContents(currentButton) {
+    // Find the closest ancestor that represents the parent accordion container
+    var parentAccordion = currentButton.closest('.accordion-container');
+
+    // Only select the opened accordion contents within this container
+    var allContents = parentAccordion.querySelectorAll('.accordion-content.opened');
     allContents.forEach(function(content) {
-      content.classList.remove('opened');
-      // Handle aria-expanded for accessibility
-      content.previousElementSibling.setAttribute('aria-expanded', 'false');
+      if (content !== currentButton.nextElementSibling) {
+        content.classList.remove('opened');
+        content.previousElementSibling.setAttribute('aria-expanded', 'false');
+      }
     });
   }
 
   accordionButtons.forEach(function(button) {
     button.addEventListener('click', function(event) {
-      console.log('clicked');
+
       var contentId = button.getAttribute('aria-controls');
       var content = document.getElementById(contentId);
       var isOpening = !content.classList.contains('opened');
 
-      // Hide all other contents
-      hideAllContents();
+      // Hide all other contents within the same accordion container
+      hideAllContents(button);
 
       if (isOpening) {
         content.classList.add('opened');
-        // Handle aria-expanded for accessibility
         button.setAttribute('aria-expanded', 'true');
       }
     });
