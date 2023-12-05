515b46a61a2989ec9b38e5b2fb61909cde549273
Simon L. Lange <sl@bellcom.dk>
2023-12-05 13:34:21 +0000

    Fredericia: URL hash accordion

--- web/themes/custom/fds_fredericia_main_theme/js/accordion.js
+++ web/themes/custom/fds_fredericia_main_theme/js/accordion.js
@@ -2,18 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
   var accordionButtons = document.querySelectorAll('.accordion-button');
 
   function hideSiblingContents(currentButton) {
-    // Check if the clicked button is a nested accordion
     var isNestedAccordion = currentButton.closest('.entity-default').parentElement.tagName === 'DIV';
 
-    // If it's a nested accordion, we don't want to close the parent
     if (isNestedAccordion) {
       return;
     }
 
-    // Get the closest 'li' ancestor to scope the siblings
     var parentListItem = currentButton.closest('li');
-
-    // Select only the sibling accordion contents within this 'li'
     var siblingAccordions = parentListItem.parentElement.querySelectorAll(':scope > li > .entity-default > .accordion-content.opened');
 
     siblingAccordions.forEach(function(content) {
@@ -24,23 +19,33 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 
+  function toggleAccordion(button, isOpening) {
+    var contentId = button.getAttribute('aria-controls');
+    var content = document.getElementById(contentId);
+
+    if (isOpening) {
+      content.classList.add('opened');
+      button.setAttribute('aria-expanded', 'true');
+      window.location.hash = contentId;
+    } else {
+      content.classList.remove('opened');
+      button.setAttribute('aria-expanded', 'false');
+    }
+  }
+
   accordionButtons.forEach(function(button) {
     button.addEventListener('click', function(event) {
-      var contentId = button.getAttribute('aria-controls');
-      var content = document.getElementById(contentId);
-      var isOpening = !content.classList.contains('opened');
-
-      // Hide all sibling contents
+      var isOpening = !button.nextElementSibling.classList.contains('opened');
       hideSiblingContents(button);
-
-      // Toggle the current accordion
-      if (isOpening) {
-        content.classList.add('opened');
-        button.setAttribute('aria-expanded', 'true');
-      } else {
-        content.classList.remove('opened');
-        button.setAttribute('aria-expanded', 'false');
-      }
+      toggleAccordion(button, isOpening);
     });
   });
+
+  if (window.location.hash) {
+    var hash = window.location.hash.substring(1); // Remove the '#' from the hash
+    var targetAccordionButton = document.querySelector(`[aria-controls="${hash}"]`);
+    if (targetAccordionButton) {
+      toggleAccordion(targetAccordionButton, true);
+    }
+  }
 });
