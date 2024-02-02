7feb01053534def8cf2a6ae32685db28aea26c60
Simon L. Lange <sl@bellcom.dk>
2024-02-02 09:40:05 +0000

    Fredericia: New search close button logic

--- web/themes/custom/fds_fredericia_main_theme/js/navigate-back.js
+++ web/themes/custom/fds_fredericia_main_theme/js/navigate-back.js
@@ -1,27 +1,29 @@
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.navigateBack = {
     attach: function (context, settings) {
-      // Updated check for multiple search pages
-      const onSearchPage = window.location.href.indexOf('/s?sq=') !== -1 ||
-        window.location.href.indexOf("/sog-selvbetjening?sq=") !== -1 ||
-        window.location.href.indexOf("/sog-dagsorden?sq=") !== -1;
+      // Function to check if the current page is one of the search pages
+      function isSearchPage(url) {
+        return url.includes('/s?sq=') || url.includes('/sog-selvbetjening?sq=') || url.includes('/sog-dagsorden?sq=');
+      }
 
-      // If not on a search page, store the current URL
-      if (!onSearchPage) {
+      // Store the current page URL if it's not a search page
+      if (!isSearchPage(window.location.href)) {
         localStorage.setItem('lastNonSearchPage', window.location.href);
       }
 
+      // Handle the close button click
       $('.search-close-btn', context).once('navigate-back').click(function (e) {
         e.preventDefault();
 
-        const storedURL = localStorage.getItem('lastNonSearchPage');
-        const referrerIsExternal = !document.referrer.includes(window.location.host);
+        // Retrieve the last non-search page URL from localStorage
+        const lastNonSearchPage = localStorage.getItem('lastNonSearchPage');
 
-        // Redirect logic
-        if (storedURL && (!referrerIsExternal || onSearchPage)) {
-          window.location.href = storedURL;
+        // Determine if there's a valid non-search page to redirect to
+        if (lastNonSearchPage) {
+          window.location.href = lastNonSearchPage;
         } else {
-          window.location.href = drupalSettings.path.baseUrl; // Navigate to the front page
+          // Redirect to the front page if no valid non-search page is found
+          window.location.href = drupalSettings.path.baseUrl;
         }
       });
     }
