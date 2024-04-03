17b24b61e83cbab40f7ef8a02dfae58d18b0c261
Henrik Tholle <ht@bellcom.dk>
2024-04-03 08:50:02 +0200

    Added rebild subsite theme

---
+++ web/themes/custom/subsites/fds_rebild_theme/src/javascripts/app.js
@@ -0,0 +1,14 @@
+jQuery(function ($) {
+  'use strict';
+});
+
+// Open all file-links in a new window.
+(function() {
+  var links = document.querySelectorAll('.field--type-file .file a');
+
+  for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+
+    link.setAttribute('target', '_blank');
+  }
+})();
---
+++ web/themes/custom/subsites/fds_rebild_theme/theme-settings.php
@@ -0,0 +1,15 @@
+<?php
+
+function fds_rebild_theme_form_system_theme_settings_alter(
+  &$form,
+  Drupal\Core\Form\FormStateInterface $form_state
+) {
+  $form['social']['#access'] = FALSE;
+
+  // Show footer logo.
+  $form['logo']['show_footer_logo'] = [
+    '#type' => 'checkbox',
+    '#title' => t('Show default footer logo.'),
+    '#default_value' => theme_get_setting('show_footer_logo'),
+  ];
+}
