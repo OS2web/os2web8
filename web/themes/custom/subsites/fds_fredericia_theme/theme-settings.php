433ab680e67aec7e0148b71fd753c84d09339435
LAPTOP-RSOLLPLO\sl <sl@bellcom.dk>
2024-10-23 08:44:31 +0200

    FR-197: Implementering af unik font

--- web/themes/custom/subsites/fds_fredericia_theme/theme-settings.php
+++ web/themes/custom/subsites/fds_fredericia_theme/theme-settings.php
@@ -20,4 +20,21 @@ function fds_fredericia_theme_form_system_theme_settings_alter(
     '#title' => t('enable '),
     '#default_value' => theme_get_setting('footer_show_latest_content'),
   ];
+
+    $font_options = [
+        'default' => t('Default Theme Font'),
+        'playfair_barlow' => t('Playfair Display for Headings, Barlow for Body Text'),
+    ];
+
+// Add a select dropdown for the font combination.
+
+    $form['fds_fredericia_theme_font_combination'] = [
+        '#type' => 'select',
+        '#title' => t('Font Combination'),
+        '#default_value' => theme_get_setting('fds_fredericia_theme_font_combination'),
+        '#options' => $font_options,
+        '#description' => t('Select the font combination for the theme.')
+    ];
+
+    return $form;
 }
