f949d2d7665855a3c21477f67dffe99b985008d3
LAPTOP-RSOLLPLO\sl <sl@bellcom.dk>
2025-07-08 15:41:26 +0200

    Fredericia: Drupal 10 friendly logic with nagivate back button

--- web/themes/custom/fds_fredericia_main_theme/js/navigate-back.js
+++ web/themes/custom/fds_fredericia_main_theme/js/navigate-back.js
@@ -1,30 +1,26 @@
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.navigateBack = {
     attach: function (context, settings) {
-      // Function to check if the current page is one of the search pages
       function isSearchPage(url) {
         return url.includes('/s?sq=') || url.includes('/sog-selvbetjening?sq=') || url.includes('/sog-dagsorden?sq=');
       }
 
-      // Store the current page URL if it's not a search page
       if (!isSearchPage(window.location.href)) {
         localStorage.setItem('lastNonSearchPage', window.location.href);
       }
 
-      // Handle the close button click
-      $('.search-close-btn', context).once('navigate-back').click(function (e) {
-        e.preventDefault();
+      once('navigate-back-button', '.search-close-btn', context).forEach(function (element) {
+        $(element).click(function (e) {
+          e.preventDefault();
 
-        // Retrieve the last non-search page URL from localStorage
-        const lastNonSearchPage = localStorage.getItem('lastNonSearchPage');
+          const lastNonSearchPage = localStorage.getItem('lastNonSearchPage');
 
-        // Determine if there's a valid non-search page to redirect to
-        if (lastNonSearchPage) {
-          window.location.href = lastNonSearchPage;
-        } else {
-          // Redirect to the front page if no valid non-search page is found
-          window.location.href = drupalSettings.path.baseUrl;
-        }
+          if (lastNonSearchPage) {
+            window.location.href = lastNonSearchPage;
+          } else {
+            window.location.href = drupalSettings.path.baseUrl;
+          }
+        });
       });
     }
   };
