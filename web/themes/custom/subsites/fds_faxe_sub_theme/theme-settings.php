e86849a2beeaae8c0677ca179662a8869e6f3591
LAPTOP-RSOLLPLO\sl <sl@bellcom.dk>
2025-09-10 13:24:43 +0200

    Faxe Kommune: Added logic for Cookie Banner on Subsite theme and Main theme, removed breadcrumbs in mobile view

--- web/themes/custom/subsites/fds_faxe_sub_theme/theme-settings.php
+++ web/themes/custom/subsites/fds_faxe_sub_theme/theme-settings.php
@@ -41,4 +41,54 @@ function fds_faxe_sub_theme_form_system_theme_settings_alter(
     '#default_value' => theme_get_setting('adgangforalle'),
     '#description' => t('Indsæt URL til genoplæsning.'),
   ];
+
+  $form['silktide_cookie_banner'] = [
+    '#type' => 'details',
+    '#title' => t('Silktide Cookie Consent Banner'),
+    '#open' => TRUE,
+  ];
+
+  // Add custom submit handler for file handling
+  $form['#submit'][] = 'fds_faxe_sub_theme_custom_theme_settings_submit';
+
+
+  $form['silktide_cookie_banner']['silktide_enabled'] = [
+    '#type' => 'checkbox',
+    '#title' => t('Aktivér Silktide Cookie Banner'),
+    '#default_value' => theme_get_setting('silktide_enabled'),
+    '#description' => t('Check denne boks for at aktivere Silktide Cookie Banner på siden.'),
+  ];
+  
+  // Add file upload field for the CSS file.
+  $form['silktide_cookie_banner']['silktide_css_fid'] = [
+    '#type' => 'managed_file',
+    '#title' => t('Silktide CSS Fil'),
+    '#description' => t('Upload Silktide CSS filen her.'),
+    '#default_value' => theme_get_setting('silktide_css_fid'),
+    '#upload_location' => 'public://silktide_assets/',
+    '#upload_validators' => [
+      'file_validate_extensions' => ['css'],
+    ],
+    '#states' => [
+      'visible' => [
+        ':input[name="silktide_enabled"]' => ['checked' => TRUE],
+      ],
+    ],
+    '#progress_message' => t('Vent venligst...'),
+  ];
+
+
+  $form['silktide_cookie_banner']['silktide_config_script'] = [
+    '#type' => 'textarea',
+    '#title' => t('Silktide Konfiguration Script'),
+    '#default_value' => theme_get_setting('silktide_config_script'),
+    '#description' => t('Indsæt Silktide script her.'),
+    '#states' => [
+      'visible' => [
+        ':input[name="silktide_enabled"]' => ['checked' => TRUE],
+      ],
+    ],
+  ];
 }
+
+
